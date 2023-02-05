0eabb99e11e87cde192fc674e36ca32667e898d3
Aritra Kar <aritrakar2002@gmail.com>
2023-02-05 13:04:12 -0500

    Modified setting of secret

--- main.py
+++ main.py
@@ -8,7 +8,8 @@ import requests
 import random
 
 # print(os.environ["OPENAI_API_KEY"])
-openai.api_key = os.environ["OPENAI_API_KEY"]
+# openai.api_key = os.environ["OPENAI_API_KEY"]
+openai.api_key = st.secrets["OPENAI_API_KEY"]
 prompt = "generate a random prompt for an ai-generated art piece"
 model = "text-davinci-003"
 n = 4
