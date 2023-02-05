cae9673cdf31dc4e8e4d0909930241638b849162
Aritra Kar <aritrakar2002@gmail.com>
2023-02-05 12:50:25 -0500

    Modified API key

--- main.py
+++ main.py
@@ -7,6 +7,7 @@ import os
 import requests
 import random
 
+# print(os.environ["OPENAI_API_KEY"])
 openai.api_key = os.environ["OPENAI_API_KEY"]
 prompt = "generate a random prompt for an ai-generated art piece"
 model = "text-davinci-003"
